f63cb160d6801d22f3bd1523e376f83d019b9516
DR497 <47689875+dr497@users.noreply.github.com>
2023-10-13 15:45:25 +0800

    python: update python bindings

--- python/src/raw_instructions.py
+++ python/src/raw_instructions.py
@@ -1,41 +1,46 @@
 from typing import List
-from borsh_construct import U8, String, CStruct
+import borsh_construct as borsh
 from solana.transaction import TransactionInstruction, AccountMeta
 from solana.publickey import PublicKey
-
-
-class ExampleInstrInstruction:
-    schema = CStruct(
-        "tag" / U8,
-        "example" / String,
-    )
-
-    def serialize(
-        self,
-        example: str,
-    ) -> str:
-        return self.schema.build(
-            {
-                "tag": 0,
-                "example": example,
-            }
-        )
-
-    def getInstruction(
-        self,
-        programId: PublicKey,
-        system_program: PublicKey,
-        spl_token_program: PublicKey,
-        fee_payer: PublicKey,
-        example_state: PublicKey,
-        example: str,
-    ) -> TransactionInstruction:
-        data = self.serialize(
-            example,
-        )
-        keys: List[AccountMeta] = []
-        keys.append(AccountMeta(system_program, False, False))
-        keys.append(AccountMeta(spl_token_program, False, False))
-        keys.append(AccountMeta(fee_payer, True, True))
-        keys.append(AccountMeta(example_state, False, True))
-        return TransactionInstruction(keys, programId, data)
+class VoteInstruction:
+	schema = borsh.CStruct(
+		"tag" / borsh.U8,
+		"user_key" / borsh.U8[32],
+		"vote_value" / borsh.U8,
+	)
+	def serialize(self,
+		user_key: List[int],
+		vote_value: int,
+	) -> str:
+		return self.schema.build({
+			"tag": 0,
+			"user_key": user_key,
+			"vote_value": vote_value,
+		})
+	def getInstruction(self,
+		user_key: List[int],
+		vote_value: int,
+programId: PublicKey,
+system_program: PublicKey,
+voter: PublicKey,
+reputation_state_account: PublicKey,
+user_vote_state_account: PublicKey,
+voter_stake_accounts: List[PublicKey],
+) -> TransactionInstruction:
+		data = self.serialize(
+		user_key,
+		vote_value,
+)
+		keys: List[AccountMeta] = []
+		keys.append(AccountMeta(system_program,
+			False, False))
+		keys.append(AccountMeta(voter,
+			True, True))
+		keys.append(AccountMeta(reputation_state_account,
+			False, True))
+		keys.append(AccountMeta(user_vote_state_account,
+			False, True))
+		for k in voter_stake_accounts:
+			keys.append(AccountMeta(k,
+			False, False))
+		return TransactionInstruction(keys, programId, data)
